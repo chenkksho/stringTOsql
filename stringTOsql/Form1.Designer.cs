ad24c19166ee5c7038956618a7a4b177d5d39516
chenkksho <530910789@qq.com>
2018-07-17 15:20:12 +0800

    新增项目

---
+++ stringTOsql/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace stringTOsql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourTxt = new System.Windows.Forms.RichTextBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sourTxt
+            // 
+            this.sourTxt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.sourTxt.Location = new System.Drawing.Point(0, 0);
+            this.sourTxt.Name = "sourTxt";
+            this.sourTxt.Size = new System.Drawing.Size(638, 395);
+            this.sourTxt.TabIndex = 0;
+            this.sourTxt.Text = "";
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(33, 412);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(184, 46);
+            this.btnChange.TabIndex = 1;
+            this.btnChange.Text = "替换";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 480);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.sourTxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox sourTxt;
+        private System.Windows.Forms.Button btnChange;
+    }
+}
+
---
+++ stringTOsql/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stringTOsql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var source = this.sourTxt.Text;
+                if(!source.Contains("sql参数：") && !source.Contains("执行sql:"))
+                {
+                    throw new Exception("格式不正确，请检查！");
+                }
+                source=source.Replace("sql参数：\n", "");
+             
+                string[] strArray= Regex.Split(source, "执行sql:");
+                if (strArray.Count()>=2)
+                {
+
+                    strArray[1] = strArray[1].Replace("\n", "");  //win7复制过来的sql不会自动换行 需要主动把换行符去掉
+                    var paramDic=cmdParam(strArray[0]);
+
+                    if (paramDic.Count <= 0) throw new Exception("参数数量不正确，请检查！");
+                    var sqlLst=cmdSql(strArray,paramDic);
+                    this.sourTxt.Text = "";
+                    foreach (var item in sqlLst)
+                    {
+                        this.sourTxt.Text += item + "\n";
+                    }
+                    
+                }
+                else
+                {
+                    throw new Exception("格式不正确，请检查！");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private List<string> cmdSql(string[] strArray, Dictionary<string, string> paramDic)
+        {
+            var strResult = new List<string>();
+            for (int i = 1; i < strArray.Length; i++)
+            {
+                var strSql = strArray[i];
+                foreach (var item in paramDic)
+                {
+                    strSql = strSql.Replace("?" + item.Key,string.Format("'{0}'", item.Value));
+                }
+                strResult.Add(strSql);
+            }
+            return strResult;
+        }
+
+        private Dictionary<string, string> cmdParam(string str)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            str= str.TrimStart('\n').TrimEnd('\n');
+            var strItems = str.Split('\n');
+            foreach (var item in strItems)
+            {
+                var itemArray = item.Split(',');
+                if (itemArray.Count() != 3) throw new Exception("参数" + itemArray[0]+"不正确，请检查！");
+                var itemValueA = Regex.Split(itemArray[2], "value:", RegexOptions.IgnoreCase);
+                if (itemValueA.Count() != 2) throw new Exception(itemArray[0]+"参数值不正确，请检查！");
+                if(dic.Keys.Contains(itemArray[0]))
+                {
+                    dic[itemArray[0]] = itemValueA[1];
+                }
+                else
+                {
+                    dic.Add(itemArray[0], itemValueA[1]);
+                }
+               
+            }
+            return dic;
+        }
+
+     
+    }
+}
